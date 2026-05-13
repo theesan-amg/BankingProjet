f000ad1a5ac4e850a306b97c8b61717a778c873c
theesan-amg <theesan27@hotmail.fr>
2026-05-13 17:50:24 +0200

    test dépôt visible

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import java.time.Clock;
+
 public class Account {
+
+    public Account(Clock clock) {
+    }
+
+    public void deposit(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+    }
+
+    public String printStatement() {
+        return String.format("%-12s %-8s %-8s%n", "Date", "Amount", "Balance");
+    }
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,3 +1,4 @@
+import org.example.Account;
 import org.junit.jupiter.api.Test;
 
 import java.time.Clock;
@@ -5,11 +6,30 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AccountTest {
 
     @Test
-    void shouldPrintHeaderWhenNoTransaction() {
+    void shouldPrintStatementWithOneDeposit() {
+        Clock clock = Clock.fixed(
+                LocalDate.of(2026, 5, 13).atStartOfDay(ZoneId.systemDefault()).toInstant(),
+                ZoneId.systemDefault()
+        );
+
+        Account account = new Account(clock);
+        account.deposit(500);
+
+        String expected =
+                String.format("%-12s %-8s %-8s%n", "Date", "Amount", "Balance") +
+                        String.format("%-12s %+8d %8d%n", "13.05.2026", 500, 500);
+
+        assertEquals(expected, account.printStatement());
+    }
+
+
+    @Test
+    void shouldThrowExceptionWhenDepositAmountIsInvalid() {
         Clock clock = Clock.fixed(
                 LocalDate.of(2026, 5, 13).atStartOfDay(ZoneId.systemDefault()).toInstant(),
                 ZoneId.systemDefault()
@@ -17,9 +37,9 @@ class AccountTest {
 
         Account account = new Account(clock);
 
-        assertEquals(
-                String.format("%-12s %-8s %-8s%n", "Date", "Amount", "Balance"),
-                account.printStatement()
-        );
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(0));
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(-10));
     }
+
+
 }
